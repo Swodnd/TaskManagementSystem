42eebea4e6c537b1a70ce5ad780d9d08ec716836
Oleksii Teliakov <swodnd@gmail.com>
2025-04-01 13:21:17 +0200

    pagination

--- src/Modules/Task.Service/Tasks/Features/GetTasks/GetTaskEntitiesEndpoint.cs
+++ src/Modules/Task.Service/Tasks/Features/GetTasks/GetTaskEntitiesEndpoint.cs
@@ -4,19 +4,21 @@ using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Shared.Pagination;
 using Tasks.Service.Tasks.Dtos;
 
 namespace Tasks.Service.Tasks.Features.GetTasks
 {
-    public record  GetTaskEntitiesResponse(List<TaskEntityDto> Tasks);
+    public record GetTaskEntitiesRequest(PaginationRequest PaginationRequest);
+    public record  GetTaskEntitiesResponse(PaginatedResult<TaskEntityDto> Tasks);
 
     public class GetTaskEntitiesEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/tasks", async (ISender sender) =>
+            app.MapGet("/tasks", async ([AsParameters] PaginationRequest request, ISender sender) =>
             {
-                var result = await sender.Send(new GetTaskEntitiesQuery());
+                var result = await sender.Send(new GetTaskEntitiesQuery(request));
                 var response = result.Adapt<GetTaskEntitiesResponse>();
 
                 return Results.Ok(response);
--- src/Modules/Task.Service/Tasks/Features/GetTasks/GetTaskEntitiesHandler.cs
+++ src/Modules/Task.Service/Tasks/Features/GetTasks/GetTaskEntitiesHandler.cs
@@ -1,26 +1,34 @@
 ﻿using Mapster;
 using Microsoft.EntityFrameworkCore;
 using Shared.CQRS;
+using Shared.Pagination;
 using Tasks.Service.Data;
 using Tasks.Service.Tasks.Dtos;
 
 namespace Tasks.Service.Tasks.Features.GetTasks
 {
-    public record GetTaskEntitiesQuery()
+    public record GetTaskEntitiesQuery(PaginationRequest PaginationRequest)
     : IQuery<GetTaskEntitiesResult>;
-    public record GetTaskEntitiesResult(IEnumerable<TaskEntityDto> Tasks);
+    public record GetTaskEntitiesResult(PaginatedResult<TaskEntityDto> Tasks);
     internal class GetTaskEntitiesHandler(TasksServiceDbContext dbContext) : IQueryHandler<GetTaskEntitiesQuery, GetTaskEntitiesResult>
     {
         public async Task<GetTaskEntitiesResult> Handle(GetTaskEntitiesQuery query, CancellationToken cancellationToken)
         {
+
+            var pageIndex = query.PaginationRequest.PageIndex;
+            var pageSize = query.PaginationRequest.PageSize;    
+            var totalCount = await dbContext.TaskEntities.LongCountAsync(cancellationToken);    
+
             var taskEntities =await dbContext.TaskEntities
                 .AsNoTracking()
                 .OrderBy(p => p.Name)
+                .Skip(pageIndex * pageSize) 
+                .Take(pageSize) 
                 .ToListAsync(cancellationToken);
 
             var taskEntityDtos = taskEntities.Adapt<List<TaskEntityDto>>();
 
-            return new GetTaskEntitiesResult(taskEntityDtos);
+            return new GetTaskEntitiesResult(new PaginatedResult<TaskEntityDto>(pageIndex, pageSize, totalCount, taskEntityDtos));
         }
     }
 }
